6390df3648fec5e0b8c3e223e20ec9a06adb30d6
Aleksandra Kornyukhina <sashadasasha@gmail.com>
2020-07-22 19:05:23 +0300

    change sql and image of structure

--- lesson2.sql
+++ lesson2.sql
@@ -42,6 +42,33 @@ ADD CONSTRAINT `fk_countries_id`
   REFERENCES `geodata`.`_countries` (`id`)
   ON DELETE RESTRICT
   ON UPDATE RESTRICT;
+  
+ALTER TABLE `geodata`.`_cities` 
+DROP COLUMN `title_cz`,
+DROP COLUMN `title_lv`,
+DROP COLUMN `title_lt`,
+DROP COLUMN `title_ja`,
+DROP COLUMN `title_pl`,
+DROP COLUMN `title_it`,
+DROP COLUMN `title_fr`,
+DROP COLUMN `title_de`,
+DROP COLUMN `title_pt`,
+DROP COLUMN `title_es`,
+DROP COLUMN `title_en`,
+DROP COLUMN `title_be`,
+DROP COLUMN `title_ua`,
+CHANGE COLUMN `city_id` `id` INT NOT NULL AUTO_INCREMENT ,
+CHANGE COLUMN `title_ru` `title` VARCHAR(150) NOT NULL ,
+ADD PRIMARY KEY (`id`),
+ADD INDEX `inx_title` (`title` ASC) INVISIBLE;
+  
+ALTER TABLE `geodata`.`_cities` 
+ADD CONSTRAINT `fk_country_id`
+  FOREIGN KEY (`country_id`)
+  REFERENCES `geodata`.`_countries` (`id`)
+  ON DELETE RESTRICT
+  ON UPDATE RESTRICT;  
+  
 
 ALTER TABLE `geodata`.`_cities` 
 ADD CONSTRAINT `fk_region_id`
